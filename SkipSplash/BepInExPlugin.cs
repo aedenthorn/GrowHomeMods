5b1cfdb92f642aaa499cf95fa598efa7c5a113dc
aedenthorn <aedenthorn1@gmail.com>
2021-09-12 08:43:43 -0400

    Add project files.

---
+++ SkipSplash/BepInExPlugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace SkipSplash
+{
+    [BepInPlugin("aedenthorn.SkipSplash", "Skip Splash Screen", "0.1.0")]
+    public partial class BepInExPlugin : BaseUnityPlugin
+    {
+        private static BepInExPlugin context;
+
+        public static ConfigEntry<bool> modEnabled;
+        public static ConfigEntry<bool> isDebug;
+
+        public static void Dbgl(string str = "", bool pref = true)
+        {
+            if (isDebug.Value)
+                Debug.Log((pref ? typeof(BepInExPlugin).Namespace + " " : "") + str);
+        }
+        private void Awake()
+        {
+
+            context = this;
+            modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+            isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug logs");
+
+            //nexusID = Config.Bind<int>("General", "NexusID", 1, "Nexus mod ID for updates");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+            Dbgl("Plugin awake");
+
+        }
+     
+        [HarmonyPatch(typeof(StartScreen), "Start")]
+        static class StartScreen_Start_Patch
+        {
+            static void Prefix()
+            {
+                if (!modEnabled.Value || StartScreen.m_skipIntros)
+                    return;
+                StartScreen.m_skipIntros = true;
+            }
+        }
+    }
+}
